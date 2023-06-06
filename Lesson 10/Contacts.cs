85750b4c0666d94e3385b04abf534fd4283d28b6
1ando11 <you@example.com>
2023-06-06 22:36:17 +0200

    Add project files.

---
+++ Lesson 10/Contacts.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_10
+{
+    class Contact : INotifyPropertyChanged
+    {
+        string name;
+        public string Name
+        {
+            get => name;
+            set
+            {
+                name = value;
+                OnPropertyChanged();
+                OnPropertyChanged(nameof(FullName));
+                OnPropertyChanged(nameof(Info));
+            }
+        }
+        string surname;
+        public string Surname
+        {
+            get => surname;
+            set
+            {
+                surname = value;
+                OnPropertyChanged();
+                OnPropertyChanged(nameof(FullName));
+                OnPropertyChanged(nameof(Info));
+            }
+        }
+        string phoneNumber;
+        public string PhoneNumber
+        {
+            get => phoneNumber;
+            set
+            {
+                phoneNumber = value;
+                OnPropertyChanged();
+                OnPropertyChanged(nameof(Info));
+            }
+        }
+        string country;
+        public string Country
+        {
+            get => country;
+            set 
+            {
+                country = value;
+                OnPropertyChanged();
+                OnPropertyChanged(nameof(Info));
+            }
+        }
+        bool isImportant;
+        public bool IsImportant
+        {
+            get => isImportant;
+            set
+            {
+                //if (isImportant == true)
+                //    isImportant = "*";
+                isImportant = value;
+                OnPropertyChanged();
+                OnPropertyChanged(nameof(Info));
+            }
+        }
+        public List<Contact> Contacts { get; set; }
+        public string FullName => Name + " " + Surname;
+        public string Info => FullName + "\n" + "+" + PhoneNumber + "\n" + Country + "\n" + "Is Important: " + "\n" + IsImportant;
+
+        public void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
---
+++ Lesson 10/MainWindow.xaml.cs
@@ -0,0 +1,106 @@
+﻿using Microsoft.VisualBasic.ApplicationServices;
+using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Lesson_10
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ObservableCollection<Contact> contacts = null;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            contacts = new ObservableCollection<Contact>();
+            {
+
+            };
+
+            ListBox.ItemsSource = contacts;
+            ListBox.DisplayMemberPath = nameof(Contact.Info);
+
+        }
+
+        private void Add_Click(object sender, RoutedEventArgs e)
+        {
+            contacts.Add(new Contact() { Name = Name.Text, Surname = Surname.Text, PhoneNumber = Phone.Text, Country = Country.Text, IsImportant = Important.IsChecked.Value });
+        }
+
+        private void Remvoe_Click(object sender, RoutedEventArgs e)
+        {
+            if (ListBox.SelectedItem != null)
+                contacts.Remove(ListBox.SelectedItem as Contact);
+        }
+
+        private void Edit_Click(object sender, RoutedEventArgs e)
+        {
+            if (ListBox.SelectedItem != null)
+            {
+                Contact c = ListBox.SelectedItem as Contact;
+                c.Name = Name.Text;
+                c.Surname = Surname.Text;
+                c.PhoneNumber = Phone.Text;
+                c.Country = Country.Text;
+                c.IsImportant = Important.IsChecked.Value;
+            }
+        }
+
+        private void Save_Click(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog save = new SaveFileDialog();
+            save.DefaultExt = ".txt";
+            save.Filter = "(.txt)|*.txt";
+            if (save.ShowDialog() == true)
+            {
+                Contact cont = ListBox.SelectedItem as Contact;
+                File.WriteAllText(save.FileName, cont.Info);
+            }
+        }
+
+        private void SortName_Click(object sender, RoutedEventArgs e)
+        {
+            if (ListBox.Items != null)
+            {
+                ListBox.ItemsSource = contacts.OrderBy(o => o.Name).ToList();
+                ListBox.Items.Refresh();
+            }
+        }
+
+        private void SortSurname_Click(object sender, RoutedEventArgs e)
+        {
+            if (ListBox.Items != null)
+            {
+                ListBox.ItemsSource = contacts.OrderBy(o => o.Surname).ToList();
+                ListBox.Items.Refresh();
+            }
+        }
+
+        private void SortCountry_Click(object sender, RoutedEventArgs e)
+        {
+            if (ListBox.Items != null)
+            {
+                ListBox.ItemsSource = contacts.OrderBy(o => o.Country).ToList();
+                ListBox.Items.Refresh();
+            }
+        }
+    }
+}
